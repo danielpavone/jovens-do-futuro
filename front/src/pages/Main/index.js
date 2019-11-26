06882695f70f664ef5f451c90e0b7137f1799a81
Leonardo Ruza <leonardoruza@Leonardos-MacBook-Pro.local>
2019-11-26 18:54:03 -0300

    New folder structure

---
+++ front/src/pages/Main/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import FeaturedCourses from '../../components/FeaturedCourses';
+import InfoData from '../../components/InfoData';
+import Footer from '../../components/Footer';
+
+function MainPage() {
+    return (
+        <>
+            <section className="banner_part">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6 col-xl-6">
+                            <div className="banner_text">
+                                <div className="banner_text_iner">
+                                    <h1>Seu conhecimento ajudando o próximo</h1>
+                                    <p>Traga sua empresa para o próximo nível e ganhe descontos compartilhando seu conhecimento.</p>
+                                    <a href="#" className="btn_1">CONHEÇA NOSSOS CURSOS</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <FeaturedCourses />
+            <InfoData />
+            <Footer />
+        </>
+    );
+}
+
+export default MainPage;

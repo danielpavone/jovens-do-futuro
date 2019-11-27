589d5d7bef9e9d933a9dc7ec356acd3b898a9b2e
Daniel Pavone <danielpavone@gmail.com>
2019-11-27 11:13:58 -0300

    remove backend

--- backend/src/models/Workshop.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const WorkshopSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  type: String
-}, { timestamps: true });
-
-module.exports = mongoose.model('Workshop', WorkshopSchema);
